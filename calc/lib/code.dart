0990e6bfbb3b125c14d91a5ba56bf11cb9990e4b
Jenil666 <jenilchandarana33@gmail.com>
2022-12-29 06:13:56 +0530

    29-12-2022

--- calc/lib/code.dart
+++ calc/lib/code.dart
@@ -9,6 +9,8 @@ class code extends StatefulWidget {
 }
 
 class _codeState extends State<code> {
+  dynamic d = 0;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -17,9 +19,17 @@ class _codeState extends State<code> {
         body: Column(
           children: [
             Container(
+              margin: EdgeInsets.only(right: 25,left: 25),
               height: 320,
               width: double.infinity,
-              //color: Colors.red,
+              alignment: Alignment.bottomRight,
+              child: Text(
+                "$d",
+                style: TextStyle(
+                  fontSize: 80,
+                  color: Colors.white,
+                ),
+              ),
             ),
             Container(
               height: 30,
@@ -32,21 +42,525 @@ class _codeState extends State<code> {
               height: 15,
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  margin: EdgeInsets.only(left: 25),
-                  height: 60,
-                  width: 150,
-                  child: Center(child: Text("AC",style: TextStyle(fontSize: 30),)),
-                  decoration: BoxDecoration(
-                      color: Color(0xffff5a66),
-                      borderRadius:
-                          BorderRadius.only(topLeft: Radius.circular(30),
-                              topRight: Radius.circular(30),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          d = 0;
+                        });
+                      },
+                      child: Container(
+                        // margin: EdgeInsets.only(left: 40),
+                        height: 60,
+                        width: 150,
+                        child: Center(
+                            child: Text(
+                          "AC",
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                        )),
+                        decoration: BoxDecoration(
+                          color: Color(0xffff5a66),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
                             bottomLeft: Radius.circular(30),
                             bottomRight: Radius.circular(30),
                           ),
+                        ),
+                      ),
+                    ),
                   ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      if(d!=0)
+                      {
+                        setState(() {
+                          d = "$d" + "%";
+                        });
+                      }
+                    },
+                    child: Container(
+                      height: 60,
+                      width: 150,
+                      child: Center(
+                        child: Text(
+                          "%",
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white38,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      if(d!=0)
+                      {
+                        setState(() {
+                          d = "$d" + "/";
+                        });
+                      }
+                    },
+                    child: Container(
+                      height: 60,
+                      width: 150,
+                      child: Center(
+                        child: Text(
+                          " /",
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Color(0xffff5a66),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "7";
+                      });
+                    }
+                    else if(d==0)
+                      {
+                        setState(() {
+                          d = "7";
+                        });
+                      }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Container(
+                      margin: EdgeInsets.only(left: 30),
+                      height: 70,
+                      width: 70,
+                      child: Center(
+                          child: Expanded(
+                        child: Text(
+                          "7",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                          ),
+                        ),
+                      )),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "8";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "8";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "8",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "9";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "9";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "9",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 90),
+                  height: 70,
+                  width: 70,
+                  child: Center(
+                      child: Text(
+                    "*",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Color(0xffff5a66),
+                    ),
+                  )),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "4";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "4";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "4",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "5";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "5";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "5",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "6";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "6";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "6",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 90),
+                  height: 70,
+                  width: 70,
+                  child: Center(
+                      child: Text(
+                    "-",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Color(0xffff5a66),
+                    ),
+                  )),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "1";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "1";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "1",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "2";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "2";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "2",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "3";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "3";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "3",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 90),
+                  height: 70,
+                  width: 70,
+                  child: Center(
+                      child: Text(
+                    "+",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Color(0xffff5a66),
+                    ),
+                  )),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "00";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "0";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "00",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + "0";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = "0";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      "0",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if(d!=0)
+                    {
+                      setState(() {
+                        d = "$d" + ".";
+                      });
+                    }
+                    else if(d==0)
+                    {
+                      setState(() {
+                        d = ".";
+                      });
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30),
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Text(
+                      ".",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    )),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 90),
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  child: Center(
+                      child: Text(
+                    "=",
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  )),
                 ),
               ],
             ),
